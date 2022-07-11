07bb4f48af31bec81174233b71f47d18eb933c7c
praveen08052010 <97897309+praveen08052010@users.noreply.github.com>
2022-07-11 19:21:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+//https://teachablemachine.withgoogle.com/models/QFh9SSMJU/https://teachablemachine.withgoogle.com/models/QFh9SSMJU/https://teachablemachine.withgoogle.com/models/QFh9SSMJU/https://teachablemachine.withgoogle.com/models/QFh9SSMJU/
